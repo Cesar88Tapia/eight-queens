f8fc87026111060e931e760a3dfb7001f8636530
Cesar Felipe Tapia Martinez <tapia.cesarfelipe88@gmail.com>
2018-12-05 14:02:29 -0600

    Update nqueens.py
    Refactoring N queens

--- nqueens.py
+++ nqueens.py
@@ -1,54 +1,18 @@
-BOARD_SIZE = 8
+solutions = []
 
-#Function to define risks within the queens
-def under_attack(column, existing_queens):
-  #Define if queen exists within the row, if not add one      
-  row = len(existing_queens) + 1
-  #Iterate through the existing queens     
-  for queen in existing_queens:
-  #Define the queen position 
-    r, c = queen
-    if r == row: return True #Checking row
-    if c == column: return True #Checking column
-    if(column - c) == (row - r): return True #Check if it's diagonal left
-    if(column - c) == -(row - r): return True #Check if it's diagonal right
-  return False
-  
-  #Create function to find solutions
-def solve(n):
-  #If n is equal to 0 return empty array for no solution
-  if n == 0: return [[]]
-  #Fucntions for smaller solutions
-  smaller_solutions = solve(n-1)
-  #Store solutions in array
-  solutions = []
-  #Iterate through solutions in smaller solutions
-  for solution in smaller_solutions:
-    #Iterate through colum´s range in the board size and increment by one
-    for column in range(1, BOARD_SIZE + 1):
-      #If there is no risk place queen
-      if not under_attack(column, solution):
-        solutions.append(solution + [(n, column)])
-        #Return solutions
-  return solutions
+def threatened(pos, past, n):
+    for i in range(len(past)):
+        if past[i] == pos or abs(pos - past[i]) == len(past) - i:
+            return True
+    return False
 
-#Create function to display solutions
-def display(solution):
-  chars = list()
-  #Iterate through row range in board size
-  for r in range(BOARD_SIZE):
-    #Iterate through colum range in board size
-    for c in range(BOARD_SIZE):
-      #If queen does not conflict
-      if (r + 1, c + 1) in solution:
-        #Place one to list
-        chars.append("1 ")
-      else:
-        #If it does conflict place 0
-        chars.append("0 ")
-    if r != BOARD_SIZE:
-      chars.append("\n")
-  return ("".join(chars))
-  
-for answer in solve(BOARD_SIZE): 
-  print(display(answer) + "\n\n")
+def nqueens(n, past=None):
+    if past == None:
+        return [nqueens(n, [i+1]) for i in range(n)]
+    if len(past) == n:
+        solutions.append(past)
+    else:
+        [nqueens(n, past + [pos]) for pos in range(1, n+1) if not threatened(pos, past, n)]
+
+nqueens(8)
+print(len(solutions))
