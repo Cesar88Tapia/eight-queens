7b630c8a1c1fa29d31b51cadd5b064ca1a9ea6ad
Cesar Felipe Tapia Martinez <tapia.cesarfelipe88@gmail.com>
2018-12-05 14:26:24 -0600

    Update nqueens.py
    Created function to identify threats and to find solutions for n size board

--- nqueens.py
+++ nqueens.py
@@ -11,13 +11,23 @@ def threatened(pos, past, n):
     #Otherwise return false
     return False
 
+#Create function to identify positions of queens
 def nqueens(n, past=None):
+    #If there is no threat(no previous position)
     if past == None:
+        #Generate first positions
         return [nqueens(n, [i+1]) for i in range(n)]
+    #If we find solution
     if len(past) == n:
+        #Store the solution into solutions
         solutions.append(past)
+        #Else find more solutions and keep storing solutions if there is no threat
     else:
         [nqueens(n, past + [pos]) for pos in range(1, n+1) if not threatened(pos, past, n)]
 
-nqueens(8)
+nqueens(8) # replace 8 with arbitrary n
 print(len(solutions))
+
+## solutions is now a list of all solutions for n number of queens
+
+#if n is 4, prints 2, if n is 8, prints 92, etc
